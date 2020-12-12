42a6e01e15ecc2febfbb4ea985e62d9192ee12d6
Alena <alena.ryazanova@mail.ru>
2020-12-12 16:46:31 +0300

    десигн, кнопочки вся фигня

--- DrawMe/Form1.Designer.cs
+++ DrawMe/Form1.Designer.cs
@@ -42,15 +42,17 @@
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(355, 87);
+            this.pictureBox1.Location = new System.Drawing.Point(355, 42);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1013, 669);
+            this.pictureBox1.Size = new System.Drawing.Size(1013, 734);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
@@ -59,117 +61,118 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 79);
+            this.button1.Location = new System.Drawing.Point(12, 121);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(192, 49);
             this.button1.TabIndex = 1;
-            this.button1.Text = "kuku";
+            this.button1.Text = "Кисть";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(53, 134);
+            this.button2.Location = new System.Drawing.Point(12, 176);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(192, 49);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Линия";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(53, 189);
+            this.button3.Location = new System.Drawing.Point(12, 231);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(192, 49);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Ломанная";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(53, 244);
+            this.button4.Location = new System.Drawing.Point(12, 286);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(192, 49);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "Прямоугольник";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(53, 299);
+            this.button5.Location = new System.Drawing.Point(12, 341);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(192, 49);
             this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
+            this.button5.Text = "Квадрат";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(53, 354);
+            this.button6.Location = new System.Drawing.Point(12, 396);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(192, 49);
             this.button6.TabIndex = 6;
-            this.button6.Text = "button6";
+            this.button6.Text = "Окружность";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(53, 409);
+            this.button7.Location = new System.Drawing.Point(12, 451);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(192, 49);
             this.button7.TabIndex = 7;
-            this.button7.Text = "button7";
+            this.button7.Text = "Эллипс";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(53, 464);
+            this.button8.Location = new System.Drawing.Point(12, 506);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(192, 49);
             this.button8.TabIndex = 8;
-            this.button8.Text = "button8";
+            this.button8.Text = "Треугольник по трем";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(53, 519);
+            this.button9.Location = new System.Drawing.Point(12, 562);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(192, 49);
             this.button9.TabIndex = 9;
-            this.button9.Text = "button9";
+            this.button9.Text = "Треугольник 90";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(53, 574);
+            this.button10.Location = new System.Drawing.Point(12, 617);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(192, 49);
             this.button10.TabIndex = 10;
-            this.button10.Text = "button10";
+            this.button10.Text = "Равнобедренный";
             this.button10.UseVisualStyleBackColor = true;
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(53, 629);
+            this.button11.Location = new System.Drawing.Point(12, 672);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(192, 49);
             this.button11.TabIndex = 11;
-            this.button11.Text = "button11";
+            this.button11.Text = "N-угольник";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(53, 684);
+            this.button12.Location = new System.Drawing.Point(12, 727);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(192, 49);
             this.button12.TabIndex = 12;
-            this.button12.Text = "button12";
+            this.button12.Text = "Правильный N-угольник";
             this.button12.UseVisualStyleBackColor = true;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(251, 534);
+            this.checkBox1.Location = new System.Drawing.Point(225, 632);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(98, 21);
             this.checkBox1.TabIndex = 13;
@@ -177,11 +180,33 @@
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(225, 121);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(112, 49);
+            this.button13.TabIndex = 14;
+            this.button13.Text = "Очистить форму";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Панель инструментов:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(1393, 798);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button13);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button11);
@@ -221,6 +246,8 @@
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label1;
     }
 }
 
